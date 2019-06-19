d392668b9473fa3930b266bc4b68b0ac3e6859a8
Ben Cooley <bcooley58@gmail.com>
2019-06-19 18:24:42 -0400

    added a timer in the js - still has some bugs, but basic functionality is there.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,61 @@
 $(document).on('click', '#start', function () {
     console.log("start!");
-    
+
 });
+
+// creating a timer to be put in the remaining time id on the html page
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function(){
+//         minutes = parseInt(timer / 60,10);
+//         seconds = parseInt(timer % 60, 10);
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+//     window.onload = function () {
+//         console.log("hi")
+//         var twoMinutes = 60*2,
+//         display = document.getElementById('#timer');
+//         startTimer(twoMinutes, display);
+//     };
+// }
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        // if less than ten seconds left, add a zero in front of the seconds
+        if (seconds < 10) {
+            $('#timer').prepend("0")
+        }
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            showResults();
+        }
+    }, 1000);
+
+
+}
+
+window.onload = function () {
+    var twoMinutes = 60 * 0.2,
+        display = document.querySelector('#timer');
+    startTimer(twoMinutes, display);
+};
 
 var answerContainers = [];
 var quizContainer = [];
-
-
 
 
 function theQuiz() {
@@ -52,9 +101,13 @@ function showResults() {
     resultsContainer.innerHTML = 'You got ' + numCorrect + ' out of ' + myQuestions.length + ' correct!!'
 }
 
+// storing references to the elements in these variables
 var quizContainer = document.getElementById("quiz");
 var resultsContainer = document.getElementById("results");
 var submitButton = document.getElementById("submit");
+
+
+// questions to be listed in order on the quiz 
 var myQuestions = [
     {
         question: "Whose family is known for celebrating Festivus?",
@@ -75,9 +128,50 @@ var myQuestions = [
             d: "Kramer"
         },
         correctAnswer: 'b'
-    }
+    },
+    {
+        question: "What action figure is shown in every episode of Seinfeld?",
+        answers: {
+            a: "Batman",
+            b: "Superman",
+            c: "The Incredible Hulk",
+            d: "Spiderman"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Who has a long running feud with their mailman, Newman?",
+        answers: {
+            a: "Jerry",
+            b: "Elaine",
+            c: "George",
+            d: "Kramer"
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Which character went into business with George's father to create a line of men's bras?",
+        answers: {
+            a: "Jerry",
+            b: "Elaine",
+            c: "George",
+            d: "Kramer"
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "Who spent time working for the New York Yankees?",
+        answers: {
+            a: "Jerry",
+            b: "Elaine",
+            c: "George",
+            d: "Kramer"
+        },
+        correctAnswer: 'c'
+    },
 ];
 
+// display the quiz
 theQuiz();
 
 $("#submitButton").on('click', showResults);
